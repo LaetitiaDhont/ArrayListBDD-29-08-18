5b293529a51a7966b974bf1a4fef7c8b06520f1e
Dhont Laetitia <laetitiadhont75@gmail.com>
2018-08-29 12:14:41 +0200

    Insert des infos

---
+++ ArrayListBDD/ressources/infos_personne.sql
@@ -0,0 +1,12 @@
+INSERT INTO `users`.`personne`
+(`nom`,
+`prenom`,
+`age`,
+`email`,
+`telephone`)
+VALUES
+('LaSaucisse','Laetis',20,'laetitiadhont75@gmail.com','Non.'),
+('Sadoun','Hamed',12,'jeteledonnepas@gmail.com','Non plus'),
+('Poulouin','Pierre',23,'exemple@yahoo.fr','Jecpas'),
+('Le','Fadi',5,'jecpas@hotmail.fr,','118218'),
+('Coin','Coin',50,'coincoin@duck.coin','231541515');
---
+++ ArrayListBDD/ressources/table_personne.sql
@@ -0,0 +1,8 @@
+USE users;
+CREATE TABLE personne (
+nom VARCHAR(50),
+prenom VARCHAR(50),
+age INT, 
+email VARCHAR(50),
+telephone VARCHAR(10)
+);
